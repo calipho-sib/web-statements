33c5ec2cfec0861d85007be507b8c02b11bc1b49
Daniel <ddtxra@gmail.com>
2016-07-13 12:04:05 +0200

    adding pretty print

--- app/org/nextprot/web/statements/controllers/Application.scala
+++ app/org/nextprot/web/statements/controllers/Application.scala
@@ -9,23 +9,26 @@ import org.nextprot.commons.statements.RawStatement
 import org.nextprot.commons.statements.StatementField
 
 import scala.collection.JavaConverters._
+import play.api.http.ContentTypes
 
 class Application extends Controller {
 
   implicit val rawStatementWrites = new Writes[RawStatement] {
     def writes(statement: RawStatement) = Json.toJson(statement.getKeyValuesMap.asScala);
   }
-  
-  def listBooks = Action {
-    Ok(Json.toJson(BedServiceStatementConverter.convert("msh2")))
-  }
 /*  
+  def listBooks = Action {
+    val json = Json.toJson(BedServiceStatementConverter.convert("msh2"));
+    Ok(Json.prettyPrint(json)).as(ContentTypes.JSON)
+  }
+  
   def statementsForEntry (entryAccession :String) = Action {
     Ok(Json.toJson(BedServiceStatementConverter.convert("msh2")))
   }*/
 
   def geneStatements (geneName :String) = Action {
-    Ok(Json.toJson(BedServiceStatementConverter.convert(geneName.toLowerCase())))
+    val json = Json.toJson(BedServiceStatementConverter.convert(geneName.toLowerCase()));
+    Ok(Json.prettyPrint(json)).as(ContentTypes.JSON)
   }
 
 }
